e47b1ced6ad2a6bc4a246084ce904951e90b76f0
Ubuntu <ubuntu@ip-172-31-41-71.us-east-2.compute.internal>
2019-11-24 23:12:36 +0000

    aplicacion corriendo correctamente

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+        Route::post('/viajes/imei_fecha','ViajeController@viajesImeiFecha');
+        
+        Route::post('/viajes/info','ViajeController@info');
+        
+        Route::get('viajes','ViajeController@index');
+        Route::delete('viajes','ViajeController@deleteAll');
+        Route::post('viajes','ViajeController@store');
+        
+        Route::get('/viajes/1', function () {
+            return new ViajeResource(Viaje::where('imei','1')->get());
+        });  
